26994bc395da63bf59c0b7bbd3817efb921c25c4
AllStarsRYello <allstarsryello@gmail.com>
2024-02-25 13:52:21 -0500

    Initial commit

---
+++ src/main/java/net/allstarsryello/starstooltips/StarsToolTipsClient.java
@@ -0,0 +1,10 @@
+package net.allstarsryello.starstooltips;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StarsToolTipsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/allstarsryello/starstooltips/StarsToolTipsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.allstarsryello.starstooltips;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StarsToolTipsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
